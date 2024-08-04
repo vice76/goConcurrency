8528b2a5ee5e590974280096f8f39a3abb481133
vice76 <arpityadavaj1999@gmail.com>
2024-08-04 12:52:46 +0530

    race check

--- mutex/main.go
+++ mutex/main.go
@@ -26,5 +26,7 @@ func main() {
 	//here , it is a problem with concurrency , as we don't have any idea
 	//when which routine is going to finish , here line no 21 got finished
 	//earlier than line no 20 , it also might be we get hello cosmos at sometime
+	//how to check whether we are having race condition
+	// go run -race .
 
 }
